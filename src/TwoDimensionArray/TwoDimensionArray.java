6aa44fbd22ad4ea061f051b3a657abf550f117bc
shubham <shubham07mathur@gmail.com>
2019-10-06 22:25:26 +0530

    Two Dimension Array

--- src/TwoDimensionArray/TwoDimensionArray.java
+++ src/TwoDimensionArray/TwoDimensionArray.java
@@ -22,7 +22,6 @@ public class TwoDimensionArray {
         try {
             for (int i = 0; i < arr.length; i++) {
                 for (int j = 0; j < arr[0].length; j++) {
-                    System.out.println(arr[0].length);
                     System.out.print(arr[i][j] + " ");
                 }
                 System.out.println();
