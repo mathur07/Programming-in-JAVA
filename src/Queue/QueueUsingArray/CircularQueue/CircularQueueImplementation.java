d740a5e85c86d2732bd48c560499da136dbb3119
Shubham Mathur <shubham07mathur@gmail.com>
2019-10-16 01:32:32 +0530

    Queue(Circular) implementation using Array

--- src/Queue/QueueUsingArray/CircularQueue/CircularQueueImplementation.java
+++ src/Queue/QueueUsingArray/CircularQueue/CircularQueueImplementation.java
@@ -10,11 +10,61 @@ public class CircularQueueImplementation {
         queue = new int[queue_size];
     }
 
-    public void en_queue() {
+    public void en_queue(int data) {
+        if (is_full()) {
+            System.out.println("Queue Is Full!, Cannot EnQueue!");
+        } else if (queue_end == queue.length - 1) {
+            queue_end = 0;
+            queue[queue_end] = data;
+        } else if (is_empty()) {
+            queue_end++;
+            queue_start++;
+            queue[queue_end] = data;
 
+        } else {
+            queue_end++;
+            queue[queue_end] = data;
+        }
     }
 
-    public void is_full() {
-        if ()
+    public void de_queue() {  //error
+        if (is_empty()) {
+            System.out.println("Queue is Empty!, Cannot DeQueue!");
+        } else if (queue_start == queue_end + 1) {
+            queue[queue_start] = Integer.MIN_VALUE;
+            queue_start = -1;
+            queue_end = -1;
+        } else {
+            queue[queue_start] = Integer.MIN_VALUE;
+            queue_start++;
+        }
+    }
+
+    public boolean is_empty() {
+        if (queue_start == -1 && queue_end == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean is_full() {
+        if (queue_start == 0 && (queue_end == queue.length - 1) || queue_end + 1 == queue_start) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void peek() {
+        System.out.println("queue_start_index = " + queue_start);
+        System.out.println("queue_end_index = " + queue_end);
+    }
+
+    public void print_queue() {
+        for (int i = 0; i < queue.length; i++) {
+            System.out.printf(queue[i] + " | ");
+        }
+        System.out.println();
     }
 }
--- src/Queue/QueueUsingArray/CircularQueue/CircularQueueMain.java
+++ src/Queue/QueueUsingArray/CircularQueue/CircularQueueMain.java
@@ -1,4 +1,63 @@
 package Queue.QueueUsingArray.CircularQueue;
 
 public class CircularQueueMain {
+    static int queue_size = 5;
+    public static void main(String[] args) {
+
+        CircularQueueImplementation q =new CircularQueueImplementation();
+
+        q.create_queue(queue_size);
+        q.print_queue();
+        q.de_queue();
+        q.en_queue(1);
+        q.print_queue();
+        q.en_queue(2);
+        q.print_queue();
+        q.peek();
+        q.en_queue(3);
+        q.peek();
+        q.print_queue();
+        q.en_queue(4);
+        q.print_queue();
+        q.en_queue(5);
+        q.print_queue();
+        q.en_queue(6);
+        q.peek();
+        q.print_queue();
+        q.de_queue();
+        q.de_queue();
+        q.de_queue();
+        q.peek();
+        q.print_queue();
+        System.out.println("=========================================");
+        q.de_queue();
+        q.print_queue();
+        q.peek();
+        q.de_queue();//<================|
+        q.print_queue();
+        q.peek();
+        q.de_queue();
+        q.peek();
+        q.print_queue();
+        q.en_queue(1);
+        q.en_queue(2);
+        q.en_queue(3);
+        q.print_queue();
+        q.de_queue();
+        q.de_queue();
+        q.peek();
+        q.print_queue();
+        q.en_queue(4);
+        q.peek();
+        q.print_queue();
+        q.de_queue();
+        q.peek();
+        q.print_queue();
+        q.de_queue();
+        q.print_queue();
+        q.de_queue();
+        q.print_queue();
+        q.peek();
+
+    }
 }
