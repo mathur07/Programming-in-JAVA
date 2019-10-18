850908fc03445d30f011a9927b60663b8c42261d
p-murli <31705134+p-murli@users.noreply.github.com>
2019-10-19 00:21:25 +0530

    Added documentation for stack methods

--- src/Stack/StackUsingArray/StackImplementation.java
+++ src/Stack/StackUsingArray/StackImplementation.java
@@ -6,12 +6,14 @@ public class StackImplementation {
 
     int[] stack;
     int top_of_stack = -1;
-
+    
+    // create a stack of capacity size_of_stack
     public void create_stack(int size_of_stack) {
         stack = new int[size_of_stack];
         Arrays.fill(stack, Integer.MIN_VALUE);
     }
 
+    // add an element to the top of the stack
     public void push(int data) {
         if (is_full()) {
             System.out.println("Stack is Full, cannot push! " + data);
@@ -20,7 +22,8 @@ public class StackImplementation {
             stack[top_of_stack] = data;
         }
     }
-
+    
+    //remove an element from the top of the stack
     public void pop() {
         if (is_empty()) {
             System.out.println("Stack is Empty, cannot pop!");
@@ -29,7 +32,8 @@ public class StackImplementation {
             top_of_stack--;
         }
     }
-
+    
+    // check if stack is empty or not
     public boolean is_empty() {
         if (top_of_stack == -1) {
             return true;
@@ -38,6 +42,7 @@ public class StackImplementation {
         }
     }
 
+    //check if stack is full
     public boolean is_full() {
         if (top_of_stack == stack.length - 1) {
             return true;
@@ -46,10 +51,12 @@ public class StackImplementation {
         }
     }
 
+    // print the topmost element of the stack
     public void peek() {
         System.out.println("Stack Top Element is = " + stack[top_of_stack]);
     }
 
+    // print all the elements in the stack 
     public void print_stack() {
         for (int i = 0; i < stack.length; i++) {
             System.out.printf(stack[i] + " | ");
