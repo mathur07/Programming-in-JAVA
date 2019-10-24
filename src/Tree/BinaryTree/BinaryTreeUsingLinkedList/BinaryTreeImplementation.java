75398dd98112fdf099981c7c374c235ef44be632
Shubham Mathur <shubham07mathur@gmail.com>
2019-10-24 15:57:30 +0530

    Binary Tree Implementation using LinkedList

--- src/Tree/BinaryTree/BinaryTreeUsingLinkedList/BinaryTreeImplementation.java
+++ src/Tree/BinaryTree/BinaryTreeUsingLinkedList/BinaryTreeImplementation.java
@@ -2,10 +2,61 @@ package Tree.BinaryTree.BinaryTreeUsingLinkedList;
 
 public class BinaryTreeImplementation {
 
-    Node head;
+    Node root;
+    Node[] queue = new Node[20];
+    int pointerIndex = 0;
+    int lastUsedIndex = 0;
 
-    void create_tree(){
+    public void create_tree() {
+        root = null;
+    }
 
+    public Node create_node(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        return newNode;
+    }
+
+    public void insert_node(int data) {
+        Node newNode = create_node(data);
+        if (is_empty()) {
+            root = newNode;
+            queue[pointerIndex] = newNode;
+        } else {
+            while (true) {
+                if (queue[pointerIndex].leftSubTree == null) {
+                    queue[pointerIndex].leftSubTree = newNode;
+                    lastUsedIndex++;
+                    queue[lastUsedIndex] = newNode;
+                    break;
+                } else if (queue[pointerIndex].rightSubTree == null) {
+                    queue[pointerIndex].rightSubTree = newNode;
+                    lastUsedIndex++;
+                    queue[lastUsedIndex] = newNode;
+                    break;
+                }
+                pointerIndex++;
+            }
+        }
+    }
+
+    public void level_order_traversal() {
+        for (int i = 0; i <= lastUsedIndex; i++) {
+            System.out.printf(queue[i].data + " ");
+        }
+        System.out.println();
+
+//        Stream.of(queue).forEach(x -> {
+//            System.out.printf(x.data + " ");
+//        });
+    }
+
+    public boolean is_empty() {
+        if (root == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- src/Tree/BinaryTree/BinaryTreeUsingLinkedList/BinaryTreeMain.java
+++ src/Tree/BinaryTree/BinaryTreeUsingLinkedList/BinaryTreeMain.java
@@ -1,4 +1,21 @@
 package Tree.BinaryTree.BinaryTreeUsingLinkedList;
 
 public class BinaryTreeMain {
+    public static void main(String[] args) {
+        BinaryTreeImplementation bt = new BinaryTreeImplementation();
+
+        bt.create_tree();
+        bt.insert_node(1);
+        bt.level_order_traversal();
+        bt.insert_node(2);
+        bt.level_order_traversal();
+        bt.insert_node(3);
+        bt.level_order_traversal();
+        bt.insert_node(4);
+        bt.level_order_traversal();
+        bt.insert_node(5);
+        bt.level_order_traversal();
+        bt.insert_node(6);
+        bt.level_order_traversal();
+    }
 }
