233b2f55f0d975de9d94a2e06f12f6ef97edb13c
Shubham Mathur <shubham07mathur@gmail.com>
2019-10-26 23:49:08 +0530

    Binary Search Tree Implementation using LinkedList

---
+++ src/Sorting/BubbleSort/BubbleSortMain.java
@@ -0,0 +1,22 @@
+package Sorting.BubbleSort;
+
+import java.util.stream.Stream;
+
+public class BubbleSortMain {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{4, 8, 5, 1, 9, 6};
+        int[] array2 = {4, 8, 5, 1, 9, 6};
+
+        System.out.println(array[0]);
+
+        Stream.of(array).forEach(i -> {
+            System.out.println("array = " + i);
+        });
+        Stream.of(array2).forEach(i -> {
+            System.out.println("array2 = " + i);
+        });
+
+    }
+}
