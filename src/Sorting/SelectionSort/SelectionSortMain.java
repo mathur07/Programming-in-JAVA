ff48589436f3d9257a2b88017f5f8b2664ec9788
Shubham Mathur <shubham07mathur@gmail.com>
2019-10-29 02:57:32 +0530

    Selection Sort Implementation on Array

--- src/Sorting/SelectionSort/SelectionSortMain.java
+++ src/Sorting/SelectionSort/SelectionSortMain.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SelectionSortMain {
     public static void main(String[] args) {
         int[] array = new int[]{4, 8, 5, 1, 9, 6};
-//        int[] array = new int[]{7, 5, 4, 2};
+//        int[] array = new int[]{7, 5, 4, 2, 1};
 
         System.out.println("Array After Sorting!");
         Arrays.stream(array).forEach(i -> {
