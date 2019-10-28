a6c0e4977fa612f74ee60707844481710549f860
Shubham Mathur <shubham07mathur@gmail.com>
2019-10-29 03:01:46 +0530

    Bucket Sort Implementation on Array

--- src/Sorting/BucketSort/BucketSortMain.java
+++ src/Sorting/BucketSort/BucketSortMain.java
@@ -1,7 +1,24 @@
 package Sorting.BucketSort;
 
+import java.util.Arrays;
+
 public class BucketSortMain {
     public static void main(String[] args) {
+        int[] array = new int[]{4, 8, 5, 1, 9, 6};
+//        int[] array = new int[]{7, 5, 4, 2, 1};
+
+        System.out.println("Array After Sorting!");
+        Arrays.stream(array).forEach(i -> {
+            System.out.print(i + " ");
+        });
+
+        System.out.println();
+
+
+        System.out.println("Array Before Sorting!");
+        Arrays.stream(array).forEach(i -> {
+            System.out.print(i + " ");
+        });
 
     }
 }
