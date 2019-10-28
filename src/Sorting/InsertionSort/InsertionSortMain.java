c976d42d769689caf8cb0683a43a59d2ef3e1bcc
Shubham Mathur <shubham07mathur@gmail.com>
2019-10-29 02:53:15 +0530

    Insertion Sort Implementation on Array

--- src/Sorting/InsertionSort/InsertionSortMain.java
+++ src/Sorting/InsertionSort/InsertionSortMain.java
@@ -6,7 +6,7 @@ public class InsertionSortMain {
     public static void main(String[] args) {
 
         int[] array = new int[]{4, 8, 5, 1, 9, 6};
-//        int[] array = new int[]{7, 5, 4, 2};
+//        int[] array = new int[]{7, 5, 4, 2, 1};
 
         System.out.println("Array After Sorting!");
         Arrays.stream(array).forEach(i -> {
--- src/Sorting/SelectionSort/SelectionSortMain.java
+++ src/Sorting/SelectionSort/SelectionSortMain.java
@@ -4,5 +4,20 @@ import java.util.Arrays;
 
 public class SelectionSortMain {
     public static void main(String[] args) {
+        int[] array = new int[]{4, 8, 5, 1, 9, 6};
+//        int[] array = new int[]{7, 5, 4, 2};
+
+        System.out.println("Array After Sorting!");
+        Arrays.stream(array).forEach(i -> {
+            System.out.print(i + " ");
+        });
+
+        System.out.println();
+
+        System.out.println("Array Before Sorting!");
+        Arrays.stream(array).forEach(i -> {
+            System.out.print(i + " ");
+        });
+
     }
 }
