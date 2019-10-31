47781bbf2a50182cadce02ca3706ebe0095dc1d5
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-01 01:25:12 +0530

    MergeSort implementation using Array

---
+++ src/Sorting/MergeSort/MergeSortImplementation.java
@@ -0,0 +1,25 @@
+package Sorting.MergeSort;
+
+import java.util.Arrays;
+
+public class MergeSortImplementation {
+    public static void main(String[] args) {
+        int[] array = new int[]{};
+
+        System.out.println("Array before Sorting! :");
+        Arrays.stream(array).forEach(i -> {
+            System.out.print(i + " ");
+        });
+
+
+
+        System.out.println("Array after Sorting! :");
+        Arrays.stream(array).forEach(i -> {
+            System.out.print(i + " ");
+        });
+    }
+
+    int merge_sort(){
+
+    }
+}
