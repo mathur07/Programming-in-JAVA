dfd033e6db11a8dfb2f18fb0c0f39087c7e07f5e
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-03 23:37:37 +0530

    Quick Sort Implementation on Array

--- src/Sorting/OuickSort/QuickSortMain.java
+++ src/Sorting/OuickSort/QuickSortMain.java
@@ -3,5 +3,27 @@ package Sorting.OuickSort;
 public class QuickSortMain {
     public static void main(String[] args) {
 
+        int[] array = new int[]{4, 8, 5, 1, 9, 6};
+
+        System.out.println("Array before Sorting : ");
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        quickSort(array);
+
+        System.out.println("Array after Sorting : ");
+        for (int value : array) {
+            System.out.printf(value + " ");
+        }
+
+    }
+
+    private static void quickSort(int[] array) {
+        int i = -1;
+        int j = 0;
+        int k = array.length - 1;
+
     }
 }
