05833e4987ff1fdf07564c200d74162390ba9d74
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-11 17:59:56 +0530

    GoMMT

---
+++ src/codeChef/Easy/LifeTheUniverseAndEverything/LifeTheUniverseAndEverything.java
@@ -0,0 +1,22 @@
+package codeChef.Easy.LifeTheUniverseAndEverything;
+
+import java.util.Scanner;
+
+public class LifeTheUniverseAndEverything {
+    public static void main(String[] args) {
+        try {
+            Scanner scan = new Scanner(System.in);
+            while (true) {
+                int temp;
+                temp = scan.nextInt();
+                if (temp == 42) {
+                    break;
+                } else {
+                    System.out.println(temp);
+                }
+            }
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
