9e9eca4fc0cd68e6f9235cb132e2181ac2f98670
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-17 02:30:12 +0530

    TwoSum

--- src/LeetCode/Easy/TwoSum/TwoSumMain.java
+++ src/LeetCode/Easy/TwoSum/TwoSumMain.java
@@ -4,7 +4,28 @@ import java.util.Scanner;
 
 public class TwoSumMain {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        int[] array = new int[]{11, 7, 11, 2};
+        int target = 9;
+        int[] result = new int[2];
+        result = twoSum(array, target);
+        for (int value : result) {
+            System.out.print(value + " ");
+        }
+    }
 
+    private static int[] twoSum(int[] array, int target) {
+        int[] result = new int[2];
+        outerLoop:
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[i] + array[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break outerLoop;
+                }
+            }
+        }
+
+        return result;
     }
 }
