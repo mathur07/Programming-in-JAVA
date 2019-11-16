1d92884022ce484816930a195c563bd605d8696e
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-16 23:40:17 +0530

    TwoSum

---
+++ src/LeetCode/Easy/TwoSum/TwoSumMain.java
@@ -0,0 +1,10 @@
+package LeetCode.Easy.TwoSum;
+
+import java.util.Scanner;
+
+public class TwoSumMain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
