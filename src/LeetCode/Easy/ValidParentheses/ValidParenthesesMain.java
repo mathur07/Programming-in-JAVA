a4eee96687234105c172a0c403be7dadfa9bfa5a
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-24 23:02:08 +0530

    Valid Parenthesis

--- src/LeetCode/Easy/ValidParentheses/ValidParenthesesMain.java
+++ src/LeetCode/Easy/ValidParentheses/ValidParenthesesMain.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class ValidParenthesesMain {
     public static void main(String[] args) {
-        String input_string = "{)}";
+        String input_string = "{{()}{";
         boolean result = valid_parentheses(input_string);
         System.out.println("result = " + result);
     }
@@ -14,22 +14,27 @@ public class ValidParenthesesMain {
 
         for (int i = 0; i < input_string.length(); i++) {
             String[] target_array = input_string.split("");
-            String target =target_array[i];
+            String target = target_array[i];
             if (("(").equals(target) || ("{").equals(target) || ("[").equals(target)) {
                 stack.push(target);
             }
-            if ((")").equals(target) || ("}").equals(target) || ("]").equals(target)) {
-                String temp = stack.pop();
-                if(target.equals(")") && !temp.equals("(")){
-                    return false;
-                }
-                if(target.equals("}") && !temp.equals("{")){
-                    return false;
-                }
-                if(target.equals("]") && !temp.equals("[")){
-                    return false;
-                }
+            if (!stack.empty()) {
+                if ((")").equals(target) || ("}").equals(target) || ("]").equals(target)) {
+                    String temp = stack.pop();
 
+                    if (target.equals(")") && !temp.equals("(")) {
+                        return false;
+                    }
+                    if (target.equals("}") && !temp.equals("{")) {
+                        return false;
+                    }
+                    if (target.equals("]") && !temp.equals("[")) {
+                        return false;
+                    }
+
+                }
+            } else {
+                return false;
             }
         }
 
