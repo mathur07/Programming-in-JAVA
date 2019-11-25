e503f56596d60e06035ffe7760cba3d7d0b53717
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-25 23:38:38 +0530

    Merge Two Sorted Lists

--- src/LeetCode/Easy/MergeTwoSortedLists/MergeTwoSortedListsMain.java
+++ src/LeetCode/Easy/MergeTwoSortedLists/MergeTwoSortedListsMain.java
@@ -14,45 +14,47 @@ public class MergeTwoSortedListsMain {
     private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
         ListNode result_head;
-        ListNode temp = null;
+        ListNode temp = new ListNode(-1);
         result_head = temp;
 
-        while (l1.next != null && l2.next != null) {
-            if (l1.val < l2.val) {
-                ListNode node = new ListNode(l1.val);
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                temp.next= new ListNode(l1.val);
                 l1 = l1.next;
             } else {
-                ListNode node = new ListNode(l2.val);
+                temp.next= new ListNode(l2.val);
                 l2 = l2.next;
             }
+            temp = temp.next;
+        }
+
+        if (l1 != null) {
+            temp.next = l1;
+
+        }
+        if (l2 != null) {
+            temp.next = l2;
 
         }
 
-        if (l1.next != null) {
 
-        }
-        if (l2.next != null) {
-
-        }
-
-
-        return result_head;
+        return result_head.next;
     }
 
     public static void main(String[] args) {
         ListNode head1, head2;
-        ListNode node_a_1 = new ListNode(2);
+        ListNode node_a_1 = new ListNode(1);
         head1 = node_a_1;
-        ListNode node_a_2 = new ListNode(3);
+        ListNode node_a_2 = new ListNode(2);
         node_a_1.next = node_a_2;
         ListNode node_a_3 = new ListNode(4);
         node_a_2.next = node_a_3;
 
-        ListNode node_b_1 = new ListNode(7);
+        ListNode node_b_1 = new ListNode(1);
         head2 = node_b_1;
-        ListNode node_b_2 = new ListNode(0);
+        ListNode node_b_2 = new ListNode(3);
         node_b_1.next = node_b_2;
-        ListNode node_b_3 = new ListNode(8);
+        ListNode node_b_3 = new ListNode(4);
         node_b_2.next = node_b_3;
 
         ListNode head = mergeTwoLists(head1, head2);
