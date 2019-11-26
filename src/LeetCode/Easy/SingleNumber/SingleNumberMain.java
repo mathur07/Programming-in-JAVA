12dc654933b946b3c110514bc8c12dadaaab5495
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-26 23:25:07 +0530

    Single Number

--- src/LeetCode/Easy/SingleNumber/SingleNumberMain.java
+++ src/LeetCode/Easy/SingleNumber/SingleNumberMain.java
@@ -1,17 +1,31 @@
 package LeetCode.Easy.SingleNumber;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SingleNumberMain {
 
     private static int singleNumber(int[] array) {
-        int result = 0;
+        int result = -1;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int value : array) {
+            if (map.get(value)!)
+        }
+        for (int value : array) {
+            if (map.get(value) == null) {
+                return value;
+            }
+        }
+
 
         return result;
     }
 
 
-
     public static void main(String[] args) {
-        int[] array = new int[]{4, 2, 2, 1, 1};
+        int[] array = new int[]{2, 4, 2, 1, 1};
         int result;
         result = singleNumber(array);
         System.out.println("result = " + result);
