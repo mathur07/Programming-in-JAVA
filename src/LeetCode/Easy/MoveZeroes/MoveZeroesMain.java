19ce87a4db18d3a7cd2ea6df0eac114dee946db7
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-27 23:41:33 +0530

    Move Zeroes

--- src/LeetCode/Easy/MoveZeroes/MoveZeroesMain.java
+++ src/LeetCode/Easy/MoveZeroes/MoveZeroesMain.java
@@ -1,14 +1,30 @@
 package LeetCode.Easy.MoveZeroes;
 
+import java.util.stream.Stream;
+
 public class MoveZeroesMain {
     public static void main(String[] args) {
         int[] array = new int[]{0, 1, 0, 3, 12};
         int[] result;
-
         result = moveZeroes(array);
+
+        for (int value : result) {
+            System.out.print(value + " ");
+        }
     }
 
     private static int[] moveZeroes(int[] array) {
 
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                for (int j = i; j < array.length - 1; j++) {
+                    temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
     }
 }
