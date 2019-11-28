f19991b438e64370a81d0d0d3d80bd0c905c765b
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-28 23:42:41 +0530

    Best Time to Buy and Sell Stock

--- src/LeetCode/Easy/BestTimetoBuyandSellStock/BestTimetoBuyandSellStockMain.java
+++ src/LeetCode/Easy/BestTimetoBuyandSellStock/BestTimetoBuyandSellStockMain.java
@@ -6,8 +6,13 @@ public class BestTimetoBuyandSellStockMain {
     private static int maxProfit(int[] array) {
         int result = -1;
 
-
-
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] - array[i] > result && array[i] < array[j]) {
+                    result = array[j] - array[i];
+                }
+            }
+        }
         return result;
     }
 
