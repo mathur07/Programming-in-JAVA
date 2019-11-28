2e1a951a57d9b8e063ed17096fe44b941f341bb6
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-28 22:26:08 +0530

    Best Time to Buy and Sell Stock

---
+++ src/LeetCode/Easy/BestTimetoBuyandSellStock/BestTimetoBuyandSellStockMain.java
@@ -0,0 +1,24 @@
+package LeetCode.Easy.BestTimetoBuyandSellStock;
+
+public class BestTimetoBuyandSellStockMain {
+
+
+    private static int maxProfit(int[] array) {
+        int result = -1;
+
+
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int result;
+        int[] array = new int[]{7, 1, 5, 3, 6, 4};
+        result = maxProfit(array);
+
+        System.out.println("result = " + result);
+    }
+
+
+}
--- src/LeetCode/Easy/MoveZeroes/MoveZeroesMain.java
+++ src/LeetCode/Easy/MoveZeroes/MoveZeroesMain.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 public class MoveZeroesMain {
     public static void main(String[] args) {
-        int[] array = new int[]{0, 1, 0, 3, 12};
+        int[] array = new int[]{0, 0, 1};
         int[] result;
         result = moveZeroes(array);
 
