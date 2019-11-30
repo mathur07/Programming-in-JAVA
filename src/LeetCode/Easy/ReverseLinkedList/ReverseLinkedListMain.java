2978713246309e1106e599d0220f465fda0c9c32
Shubham Mathur <shubham07mathur@gmail.com>
2019-11-30 23:14:32 +0530

    Reverse Linked List

--- src/LeetCode/Easy/ReverseLinkedList/ReverseLinkedListMain.java
+++ src/LeetCode/Easy/ReverseLinkedList/ReverseLinkedListMain.java
@@ -16,13 +16,19 @@ public class ReverseLinkedListMain {
     private static ListNode reverseList(ListNode head) {
         ListNode temp;
         ListNode temp2;
-        temp = head.next;
-        while (head.next != null && temp.next != null) {
+        temp = head;
+        head=null;
+        temp = temp.next;
+        while (temp != null) {
+            if (temp.next==null){
+                break;
+            }
             temp2 = temp.next;
             temp.next = head;
             head = temp;
             temp = temp2;
         }
+
 
         return head;
     }
@@ -35,9 +41,10 @@ public class ReverseLinkedListMain {
         node_a_1.next = node_a_2;
         ListNode node_a_3 = new ListNode(4);
         node_a_2.next = node_a_3;
+        ListNode node_a_4 = new ListNode(5);
+        node_a_3.next = node_a_4;
 
         ListNode head = reverseList(head1);
-        System.out.print("hi");
         while (head.next != null) {
             System.out.print(head.val + " -> ");
             head = head.next;
