dd6899ec32f1f12942312746cfc3a7397662047c
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-03 23:31:54 +0530

    MajorityElement

---
+++ src/LeetCode/Easy/MajorityElement/MajorityElementMain.java
@@ -0,0 +1,30 @@
+package LeetCode.Easy.MajorityElement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElementMain {
+
+
+    private static int majorityElement(int[] nums) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int num : nums) {
+            if (hashMap.get(num) != null) {
+                int value = hashMap.get(num);
+                value++;
+                hashMap.put(num, value);
+            } else {
+                hashMap.put(num, 1);
+            }
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 2, 3};
+        int result;
+        result = majorityElement(nums);
+    }
+
+}
