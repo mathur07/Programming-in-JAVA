1de24337279ade460790e7bbcc6f0ee4dd537a2a
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-08 21:54:29 +0530

    Climbing Stairs

--- src/LeetCode/Easy/ClimbingStairs/ClimbingStairMain.java
+++
@@ -1,7 +0,0 @@
-package LeetCode.Easy.ClimbingStairs;
-
-public class ClimbingStairMain {
-    public static void main(String[] args) {
-
-    }
-}
--- src/LeetCode/Easy/ClimbingStairs/ClimbingStairsMain.java
+++ src/LeetCode/Easy/ClimbingStairs/ClimbingStairsMain.java
@@ -1,1 +1,16 @@
+package LeetCode.Easy.ClimbingStairs;
 
+public class ClimbingStairsMain {
+
+
+    private static int climbStairs(int n) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, result;
+        result = climbStairs(n);
+        System.out.println("n = " + n);
+    }
+}
