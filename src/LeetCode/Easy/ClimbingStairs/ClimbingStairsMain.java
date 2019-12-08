c2a96bada673ed0272b7361f112e41b7216f453f
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-08 23:35:27 +0530

    Climbing Stairs

--- src/LeetCode/Easy/ClimbingStairs/ClimbingStairsMain.java
+++ src/LeetCode/Easy/ClimbingStairs/ClimbingStairsMain.java
@@ -4,12 +4,26 @@ public class ClimbingStairsMain {
 
 
     private static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
 
-        return 0;
+        int[] tableArray = new int[n];
+        tableArray[0] = 1;
+        tableArray[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            tableArray[i] = tableArray[i - 1] + tableArray[i - 2];
+        }
+
+    return tableArray[n-1];
     }
 
     public static void main(String[] args) {
-        int n = 3, result;
+        int n = 5, result;
         result = climbStairs(n);
         System.out.println("result = " + result);
     }
