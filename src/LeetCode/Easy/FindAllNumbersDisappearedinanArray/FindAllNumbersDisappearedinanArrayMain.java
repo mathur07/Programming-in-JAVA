e0b3164a093a66aa5e83ad683ee4851edbdc08cd
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-09 23:31:53 +0530

    Find All Numbers Disappeared in an Array

---
+++ src/LeetCode/Easy/FindAllNumbersDisappearedinanArray/FindAllNumbersDisappearedinanArrayMain.java
@@ -0,0 +1,20 @@
+package LeetCode.Easy.FindAllNumbersDisappearedinanArray;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedinanArrayMain {
+    public static void main(String[] args) {
+        int[] nums = new int[]{4, 3, 2, 7, 8, 2, 3, 1};
+        List<Integer> list;
+        list = findDisappearedNumbers(nums);
+        System.out.println("list = " + list);
+    }
+
+    private static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new LinkedList<>();
+
+        return list;
+
+    }
+}
