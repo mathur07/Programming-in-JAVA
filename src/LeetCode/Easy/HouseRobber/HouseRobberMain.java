ea8fd3d82b8eac83f866c405aa30ac8412978751
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-17 23:29:51 +0530

    House Robber

---
+++ src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
@@ -0,0 +1,14 @@
+package LeetCode.Easy.HouseRobber;
+
+public class HouseRobberMain {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result;
+        result = rob(nums);
+        System.out.println("result = " + result);
+    }
+
+    private static int rob(int[] nums) {
+        return 0;
+    }
+}
