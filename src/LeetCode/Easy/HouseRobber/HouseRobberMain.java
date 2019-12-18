bed1d562d3a3631fe7a49b5ae2c2433d8b4f1afc
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-18 23:47:10 +0530

    House Robber

--- src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
+++ src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
@@ -1,14 +1,28 @@
 package LeetCode.Easy.HouseRobber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HouseRobberMain {
     public static void main(String[] args) {
-        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] nums = new int[]{2, 7, 9, 3, 1};
         int result;
         result = rob(nums);
         System.out.println("result = " + result);
     }
 
     private static int rob(int[] nums) {
+        List<Integer> even = new ArrayList<Integer>();
+        List<Integer> odd = new ArrayList<Integer>();
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (i % 2 == 1) {
+                odd.add(nums[i]);
+            } else {
+                even.add(nums[i]);
+            }
+        }
+
         return 0;
     }
 }
