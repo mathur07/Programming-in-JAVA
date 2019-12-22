c2c4aea45b365aebcb3e5792f9257536a1c3589c
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-22 23:45:22 +0530

    House Robber

--- src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
+++ src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
@@ -1,15 +1,1 @@
-package LeetCode.Easy.HouseRobber;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class HouseRobberMain {
-    public static void main(String[] args) {
-
-    }
-
-    private static int rob(int[] nums) {
-
-        return 0;
-    }
-}
