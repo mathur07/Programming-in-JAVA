5dabe8d835f81440b4252a2e319ecdc4662183f0
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-24 00:37:29 +0530

    House Robber

--- src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
+++ src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
@@ -1,28 +1,35 @@
 package LeetCode.Easy.HouseRobber;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class HouseRobberMain {
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 9, 3, 1};
+        int[] nums = new int[]{2, 7, 9, 4, 1};
         int result;
         result = rob(nums);
         System.out.println("result = " + result);
     }
 
     private static int rob(int[] nums) {
-        List<Integer> even = new ArrayList<Integer>();
-        List<Integer> odd = new ArrayList<Integer>();
 
-        for (int i = 1; i <= nums.length; i++) {
-            if (i % 2 == 1) {
-                odd.add(nums[i]);
-            } else {
-                even.add(nums[i]);
-            }
+        int[] temp_array = new int[nums.length];
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
         }
 
-        return 0;
+        if (nums.length == 2) {
+            return Integer.max(nums[0], nums[1]);
+        }
+
+        temp_array[0] = nums[0];
+        temp_array[1] = nums[1];
+
+        for (int i = 2; i < temp_array.length; i++) {
+            temp_array[i] = nums[i] + temp_array[i - 2];
+        }
+
+
+        return Integer.max(temp_array[temp_array.length - 1], temp_array[temp_array.length - 2]);
     }
 }
