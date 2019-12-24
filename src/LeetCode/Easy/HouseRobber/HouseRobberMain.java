b2507a196b3527e4af47b0117cf3801f2b83aea7
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-25 00:00:33 +0530

    House Robber

--- src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
+++ src/LeetCode/Easy/HouseRobber/HouseRobberMain.java
@@ -2,10 +2,10 @@ package LeetCode.Easy.HouseRobber;
 
 public class HouseRobberMain {
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 9, 4, 1};
-        int result;
-        result = rob(nums);
-        System.out.println("result = " + result);
+            int[] nums = new int[]{2, 7, 9, 4, 1};
+            int result;
+            result = rob(nums);
+            System.out.println("result = " + result);
     }
 
     private static int rob(int[] nums) {
@@ -26,7 +26,7 @@ public class HouseRobberMain {
         temp_array[1] = Integer.max(nums[0], nums[1]);
 
         for (int i = 2; i < temp_array.length; i++) {
-            temp_array[i] = Integer.max(temp_array[i - 1], temp_array[i] + temp_array[i - 2]);
+            temp_array[i] = Integer.max(temp_array[i - 1], nums[i] + temp_array[i - 2]);
         }
 
         return temp_array[temp_array.length - 1];
---
+++ src/LeetCode/Easy/ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarrayMain.java
@@ -0,0 +1,14 @@
+package LeetCode.Easy.ShortestUnsortedContinuousSubarray;
+
+public class ShortestUnsortedContinuousSubarrayMain {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 6, 4, 8, 10, 9, 15};
+        int result;
+        result = findUnsortedSubarray(nums);
+        System.out.println("result = " + result);
+    }
+
+    private static int findUnsortedSubarray(int[] nums) {
+        return 0;
+    }
+}
