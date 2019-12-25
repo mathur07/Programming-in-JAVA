372ba1b9f74931e8aeb9050f272813e3c0d95d2a
Shubham Mathur <shubham07mathur@gmail.com>
2019-12-26 01:13:57 +0530

    Find Unsorted Subarray

--- src/LeetCode/Easy/ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarrayMain.java
+++ src/LeetCode/Easy/ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarrayMain.java
@@ -2,14 +2,26 @@ package LeetCode.Easy.ShortestUnsortedContinuousSubarray;
 
 public class ShortestUnsortedContinuousSubarrayMain {
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 6, 4, 8, 10, 9, 15};
+        int[] nums = new int[]{2, 6, 4, 8, 10, 9, 15, 20, 7};
         int result;
         result = findUnsortedSubarray(nums);
         System.out.println("result = " + result);
     }
 
     private static int findUnsortedSubarray(int[] nums) {
-        // code
+        int min, max, min_i, max_i;
+        min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i] && min > nums[i]) {
+                min = nums[i];
+            }
+            if (nums[i - 1] > nums[i] && max < nums[i]) {
+                max = nums[i];
+            }
+        }
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
         return 0;
     }
 }
