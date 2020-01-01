b7793d68b7b30971b40051bdbe850c4e3e597e3c
Shubham Mathur <shubham07mathur@gmail.com>
2020-01-01 23:15:36 +0530

    Counting Bits

---
+++ src/LeetCode/Medium/AddTwoNumbers/CountingBits/CountingBitsMain.java
@@ -0,0 +1,22 @@
+package LeetCode.Medium.AddTwoNumbers.CountingBits;
+
+
+public class CountingBitsMain {
+    public static void main(String[] args) {
+        int num = 5;
+        int[] result;
+        result = countBits(num);
+        for (int number:result) {
+            System.out.print(number + " ");
+        }
+    }
+
+    private static int[] countBits(int num) {
+        int[] result = new int[num+1];
+
+        //logic
+
+        return result;
+
+    }
+}
