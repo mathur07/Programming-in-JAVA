bed8f3f3528fd28d943de3c29e0231eca197bfd6
Shubham Mathur <shubham07mathur@gmail.com>
2020-01-06 23:47:04 +0530

    Contains Duplicate

---
+++ src/LeetCode/Easy/ContainsDuplicate/ContainsDuplicateMain.java
@@ -0,0 +1,15 @@
+package LeetCode.Easy.ContainsDuplicate;
+
+public class ContainsDuplicateMain {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 1};
+        boolean result;
+
+        result = containsDuplicate(array);
+        System.out.println("result = " + result);
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        return false;
+    }
+}
