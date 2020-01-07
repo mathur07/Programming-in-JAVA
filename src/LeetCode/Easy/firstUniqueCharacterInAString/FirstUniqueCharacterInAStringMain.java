c765a55a19919a8ceb989e4cf227682e7772ee2c
Shubham Mathur <shubham07mathur@gmail.com>
2020-01-07 23:43:30 +0530

    First Uniq Char

---
+++ src/LeetCode/Easy/firstUniqueCharacterInAString/FirstUniqueCharacterInAStringMain.java
@@ -0,0 +1,14 @@
+package LeetCode.Easy.firstUniqueCharacterInAString;
+
+public class FirstUniqueCharacterInAStringMain {
+    public static void main(String[] args) {
+        String str = "leetcode";
+        int result ;
+        result = firstUniqChar(str);
+        System.out.println("result = " + result);
+    }
+
+    private static int firstUniqChar(String str) {
+        return 0;
+    }
+}
