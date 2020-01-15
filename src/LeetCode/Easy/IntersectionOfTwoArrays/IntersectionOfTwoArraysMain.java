c6305938d51ce261a0b7ce5a0904997d7b7f207c
Shubham Mathur <shubham07mathur@gmail.com>
2020-01-15 23:07:42 +0530

    Intersection Of Two Arrays

--- src/LeetCode/Easy/IntersectionOfTwoArrays/IntersectionOfTwoArraysMain.java
+++ src/LeetCode/Easy/IntersectionOfTwoArrays/IntersectionOfTwoArraysMain.java
@@ -6,16 +6,16 @@ public class IntersectionOfTwoArraysMain {
         int[] array1 = {1, 2, 2, 1};
         int[] array2 = {2, 2};
 
-        result = intersect(array1, array2);
-
-        for (int val : result) {
-            System.out.println(val);
-
-        }
-    }
-
-    private static int[] intersect(int[] nums1, int[] nums2) {
-
-        return new int[0];
+//        result = intersect(array1, array2);
+//
+//        for (int val : result) {
+//            System.out.println(val);
+//
+//        }
+//    }
+//
+//    private static int[] intersect(int[] nums1, int[] nums2) {
+//
+//        return new int[0];
     }
 }
