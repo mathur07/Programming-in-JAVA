4dedf3bd5cebbdb6ab7e8f33b81c3b5f6f8d6b9e
Shubham Mathur <shubham07mathur@gmail.com>
2020-01-18 23:33:00 +0530

    Intersection Of Two Arrays

--- src/LeetCode/Easy/IntersectionOfTwoArrays/IntersectionOfTwoArraysMain.java
+++ src/LeetCode/Easy/IntersectionOfTwoArrays/IntersectionOfTwoArraysMain.java
@@ -6,9 +6,9 @@ import java.util.Map;
 
 public class IntersectionOfTwoArraysMain {
     public static void main(String[] args) {
-        ArrayList<Integer> result;
-        int[] array1 = {1, 2, 2, 1};
-        int[] array2 = {2, 2};
+        int[] result;
+        int[] array1 = {4, 9, 5};
+        int[] array2 = {9, 4, 9, 8, 4};
 
         result = intersect(array1, array2);
 
@@ -17,7 +17,7 @@ public class IntersectionOfTwoArraysMain {
         }
     }
 
-    private static ArrayList<Integer> intersect(int[] nums1, int[] nums2) {
+    private static int[] intersect(int[] nums1, int[] nums2) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
         ArrayList<Integer> array = new ArrayList<>();
@@ -32,16 +32,23 @@ public class IntersectionOfTwoArraysMain {
             map.put(num, temp);
         }
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 0) {
-                temp = entry.getValue();
-                while (temp != 0) {
-                    array.add(entry.getKey());
-                    temp--;
-                }
-            }
+//        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//            if (entry.getValue() > 0) {
+//                temp = entry.getValue();
+//                while (temp != 0) {
+//                    array.add(entry.getKey());
+//                    temp--;
+//                }
+//            }
+//        }
+
+        int[] retArray = new int[array.size()];
+
+        for (int i = 0; i < array.size(); i++) {
+            retArray[i] = array.get(i);
+
         }
 
-        return array;
+        return retArray;
     }
 }
