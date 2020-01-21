b40b76b4f46e597c721cfae0ce4922461d0e4d1e
Shubham Mathur <shubham07mathur@gmail.com>
2020-01-21 22:54:42 +0530

    Pascals Triangle

---
+++ src/LeetCode/Easy/PascalsTriangle/PascalsTriangleMain.java
@@ -0,0 +1,21 @@
+package LeetCode.Easy.PascalsTriangle;
+
+import java.util.List;
+
+public class PascalsTriangleMain {
+    public static void main(String[] args) {
+        int numRows = 5;
+        List<List<Integer>> list;
+        list = generate(numRows);
+        System.out.println("list = " + list);
+    }
+
+    private static List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> resultList = new List<List<Integer>>();
+
+        return resultList;
+
+    }
+
+}
