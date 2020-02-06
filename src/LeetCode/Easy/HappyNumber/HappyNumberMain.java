091def28b295b98afae208117c5d0ddab91b34aa
Shubham Mathur <shubham07mathur@gmail.com>
2020-02-07 00:56:52 +0530

    Happy Number

--- src/LeetCode/Easy/HappyNumber/HappyNumberMain.java
+++ src/LeetCode/Easy/HappyNumber/HappyNumberMain.java
@@ -3,21 +3,31 @@ package LeetCode.Easy.HappyNumber;
 public class HappyNumberMain {
     public static void main(String[] args) {
 
-        int num = 58;
+        int num = 19;
         boolean result;
         result = isHappy(num);
         System.out.println("result = " + result);
 
-
     }
 
     private static boolean isHappy(int num) {
-        int sum=-1;
-        while (sum != 1){
-            while (){
-
+        int sum = 0;
+        try {
+            while (true) {
+                while (num > 0) {
+                    sum += Math.pow(num % 10, 2);
+                    num = num / 10;
+                }
+                num = sum;
+                if (sum == 1) {
+                    return true;
+                } else {
+                    sum = 0;
+                }
             }
+        } catch (Exception e) {
+            return false;
         }
-        return false;
+
     }
 }
