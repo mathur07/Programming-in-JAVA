6f2b00fb6e5691eaab23094ec6b24149ff78f63d
bob <shubham07mathur@gmailcom>
2020-02-17 23:46:19 +0530

    Permutations

--- src/LeetCode/Medium/Permutations/PermutationsMain.java
+++ src/LeetCode/Medium/Permutations/PermutationsMain.java
@@ -1,7 +1,16 @@
 package LeetCode.Medium.Permutations;
 
+import java.util.List;
+
 public class PermutationsMain {
     public static void main(String[] args) {
         int[] input = new int[]{1,2,3};
+        List<Object> resultlist;
+
+        resultlist = permute(input);
+    }
+
+    private static List<Object> permute(int[] input) {
+
     }
 }
