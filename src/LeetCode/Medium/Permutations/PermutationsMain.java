b3947d474731d2e7aba69ac46da5be116275c36f
bob07 <shubham07mathur@gmailcom>
2020-02-22 23:45:17 +0530

    Permutations
    Signed-off-by: bob07 <shubham07mathur@gmailcom>

---
+++ src/LeetCode/Medium/Permutations/PermutationsMain.java
@@ -0,0 +1,13 @@
+package LeetCode.Medium.Permutations;
+
+import java.util.List;
+
+public class PermutationsMain {
+    public static void main(String[] args) {
+        int[] input = new int[]{1,2,3};
+
+    }
+
+
+}
+
