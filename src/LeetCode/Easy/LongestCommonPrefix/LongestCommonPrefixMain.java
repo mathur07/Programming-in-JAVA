d2e5893d024eb60065d1818e3d4f12a6903ee40e
bob07 <shubham07mathur@gmailcom>
2020-02-23 01:00:37 +0530

    longest Common Prefix
    Signed-off-by: bob07 <shubham07mathur@gmailcom>

---
+++ src/LeetCode/Easy/LongestCommonPrefix/LongestCommonPrefixMain.java
@@ -0,0 +1,14 @@
+package LeetCode.Easy.LongestCommonPrefix;
+
+public class LongestCommonPrefixMain {
+    public static void main(String[] args) {
+        String[] input = new String[]{"flower", "flow", "flight"};
+        String result;
+        result = longestCommonPrefix(input);
+        System.out.println("result = " + result);
+    }
+
+    private static String longestCommonPrefix(String[] input) {
+        return null;
+    }
+}
