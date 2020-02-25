1e14479a17f2ae670848ca6e6c3df2123c34f84e
bob07 <shubham07mathur@gmailcom>
2020-02-26 01:47:49 +0530

    longest Common Prefix

--- src/LeetCode/Easy/LongestCommonPrefix/LongestCommonPrefixMain.java
+++ src/LeetCode/Easy/LongestCommonPrefix/LongestCommonPrefixMain.java
@@ -2,24 +2,31 @@ package LeetCode.Easy.LongestCommonPrefix;
 
 public class LongestCommonPrefixMain {
     public static void main(String[] args) {
-        String[] input = new String[]{"flower", "flow", "flight"};
+        String[] input = new String[]{"a"};
         String result;
         result = longestCommonPrefix(input);
         System.out.println("result = " + result);
     }
 
     private static String longestCommonPrefix(String[] input) {
+        if (input.length == 0) {
+            return "";
+        }
+        System.out.println("input.length = " + input.length);
         int n = input[0].length();
-        char temp =  input[0].charAt(0);
+        char temp;
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n; i++) {
+            temp = input[0].charAt(i);
             for (String s : input) {
+                if (s.length() < n) {
+                    n = s.length();
+                }
                 if (temp != s.charAt(i)) {
                     return result.toString();
                 }
-                result.append(temp);
-                temp = s.charAt(i);
             }
+            result.append(temp);
         }
         return result.toString();
 
