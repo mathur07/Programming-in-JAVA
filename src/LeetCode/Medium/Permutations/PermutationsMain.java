031517c088565617676eb5f943618ea973d39c37
Shubham Mathur <shubham07mathur@gmail.com>
2020-03-15 22:39:34 +0530

    Permutations

--- src/LeetCode/Medium/Permutations/PermutationsMain.java
+++ src/LeetCode/Medium/Permutations/PermutationsMain.java
@@ -1,7 +1,7 @@
 package LeetCode.Medium.Permutations;
 
 public class PermutationsMain {
-    public static void main(String[] args) {
+   // public static void main(String[] args) {
 
-    }
+    //}
 }
